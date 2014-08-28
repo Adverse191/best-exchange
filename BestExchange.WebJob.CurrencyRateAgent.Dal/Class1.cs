9d2590c1c316519b81291ccae9779f83ba5be5a6
unknown <Yury_Stsiapanau@EPBYBREW0131.minsk.epam.com>
2014-08-28 19:01:07 +0300

    Added data access layer for CurrencyRateAgent

---
+++ BestExchange.WebJob.CurrencyRateAgent.Dal/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestExchange.WebJob.CurrencyRateAgent.Dal
+{
+    public class Class1
+    {
+    }
+}
